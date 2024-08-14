2f39f132b08158d9fc9ecb4cd0e436aaddf5c884
bcdevon <brady.devon0@gmail.com>
2024-08-13 19:16:45 -0700

    adding product

--- app/src/main/java/com/example/bicycleshop/UI/ProductDetails.java
+++ app/src/main/java/com/example/bicycleshop/UI/ProductDetails.java
@@ -2,7 +2,10 @@ package com.example.bicycleshop.UI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,9 +17,17 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.bicycleshop.R;
 import com.example.bicycleshop.database.Repository;
+import com.example.bicycleshop.entities.Product;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ProductDetails extends AppCompatActivity {
+    String name;
+    double price;
+    int productID;
+    EditText editName;
+    EditText editPrice;
+
+
     Repository repository;
 
     @Override
@@ -24,6 +35,15 @@ public class ProductDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
         FloatingActionButton fab=findViewById(R.id.floatingActionButton2);
+
+        editName=findViewById(R.id.titletext);
+        editPrice=findViewById(R.id.pricetext);
+        productID = getIntent().getIntExtra("id", -1);
+        name = getIntent().getStringExtra("name");
+        price = getIntent().getDoubleExtra("price", 0.0);
+        editName.setText(name);
+        editPrice.setText(Double.toString(price));
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,5 +57,19 @@ public class ProductDetails extends AppCompatActivity {
         recyclerView.setAdapter(partAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         partAdapter.setParts(repository.getmAllParts());
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_product_details, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId()== R.id.productsave){
+            Product product;
+            if (productID==-1){
+                if (repository.getmAllProducts().size() == 0)productID = 1;
+                else productID = repository.getmAllProducts().get(repository.getmAllProducts().size() -1).getProductID() +1;
+                product=
+            }
+        }
     }
 }
