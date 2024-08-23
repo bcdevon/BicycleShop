0bdbfa2dc6cb9a519ca35e8ff36665d4649156e0
bcdevon <brady.devon0@gmail.com>
2024-08-23 10:49:17 -0700

    project working

--- app/src/main/java/com/example/bicycleshop/UI/PartDetails.java
+++ app/src/main/java/com/example/bicycleshop/UI/PartDetails.java
@@ -26,7 +26,6 @@ public class PartDetails extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_part_details);
 
         editPartName = findViewById(R.id.editPartName);
